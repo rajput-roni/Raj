c96e2321efc5118e328efe742a9ffd3ec8fa4023
rajput-roni <rajnath37857@gmail.com>
2025-04-17 07:42:39 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("save").addEventListener("click", () => {
-  const message = document.getElementById("message").value;
-  chrome.storage.local.set({ autoMessage: message }, () => {
-    alert("Message saved!");
-  });
-});
